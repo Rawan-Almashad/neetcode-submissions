3b04680842bbe5281e8c3113ec3b09fb818a30f3
Rawan Almashad <rawanalmashad@gmail.com>
2026-05-29 12:56:59 +0300

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int mini=1006;
+        int ans=0;
+        for(int i=0;i<prices.length;i++)
+        {
+            mini=Math.min(mini,prices[i]);
+            ans=Math.max(ans,prices[i]-mini);
+        }
+        return ans;
+    }
+}
